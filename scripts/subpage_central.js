a590c0ba3be0c2ccb0539203599171a560adc933
Arcywojak <maciej778@op.pl>
2020-08-03 21:16:25 +0200

    some changes

--- scripts/subpage_central.js
+++ scripts/subpage_central.js
@@ -1,6 +1,6 @@
 
 
-const fourthBlockNav = document.querySelector(".content-block__fourth-nav");
+const fourthBlockNav = document.querySelector(".content-block__nav.fourth");
 
 fourthBlockNav.childNodes.forEach( navItem => {
 
@@ -29,8 +29,46 @@ fourthBlockNav.childNodes.forEach( navItem => {
         fourthBlockWrapper.style.transform = `translateX(${moveValue}%)`
 
         /* Remove earlier active options */
-        if(document.querySelector(".flying-nav-central.active") ){
-            const activeBlock = document.querySelector(".flying-nav-central.active");
+        if(document.querySelector(".flying-nav-central.fourth-section.active") ){
+            const activeBlock = document.querySelector(".flying-nav-central.fourth-section.active");
+
+            activeBlock.classList.remove("active");
+        }
+
+        e.target.classList.add("active");
+        
+    })
+    
+})
+
+const secondBlockNav = document.querySelector(".content-block__nav.second");
+
+secondBlockNav.childNodes.forEach( navItem => {
+
+    const secondBlockWrapper = document.querySelector(".content-block__second-wrapper")
+
+    navItem.addEventListener("click", (e) => {
+
+        let moveValue;
+    
+        switch(e.target.id) {
+
+            case "first":
+              moveValue = 0;
+              break;
+            case "second":
+                moveValue = -100;
+                break;
+            default :
+            moveValue = 0;
+            break;
+        }
+
+        secondBlockWrapper.style.transform = `translateX(${moveValue}%)`
+
+        /* Remove earlier active options */
+        if(document.querySelector(".flying-nav-central.second-section.active") ){
+            const activeBlock = document.querySelector(".flying-nav-central.second-section.active");
 
             activeBlock.classList.remove("active");
         }
